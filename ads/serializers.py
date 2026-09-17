c96719b47d7d7c729b415a42543be49e1f8a6c9c
dr-Panakhov <rafael.panaxov14@gmail.com>
2026-09-17 22:51:24 +0400

    fix photo

--- ads/serializers.py
+++ ads/serializers.py
@@ -8,9 +8,14 @@ class AdImageSerializer(serializers.ModelSerializer):
 
 class AdSerializer(serializers.ModelSerializer):
     images = AdImageSerializer(many=True, read_only=True)
-    # Специальное поле, чтобы ловить файлы с фронтенда
     uploaded_images = serializers.ListField(
         child=serializers.ImageField(allow_empty_file=False, use_url=False),
+        write_only=True,
+        required=False
+    )
+    # НОВОЕ ПОЛЕ: список ID фоток, которые юзер хочет удалить
+    deleted_images = serializers.ListField(
+        child=serializers.IntegerField(),
         write_only=True,
         required=False
     )
@@ -18,29 +23,30 @@ class AdSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Ad
-        # Не забудь, что тут теперь есть и images, и uploaded_images!
-        fields = ['id', 'title', 'city', 'phone_number', 'description', 'price', 'currency', 'author', 'created_at', 'images', 'uploaded_images']
+        # Не забудь добавить deleted_images сюда!
+        fields = ['id', 'title', 'city', 'phone_number', 'description', 'price', 'currency', 'author', 'created_at', 'images', 'uploaded_images', 'deleted_images']
 
-    # Метод для СОЗДАНИЯ объявления (POST)
     def create(self, validated_data):
         uploaded_images = validated_data.pop('uploaded_images', [])
         ad = Ad.objects.create(**validated_data)
-        
         for image in uploaded_images:
             AdImage.objects.create(ad=ad, image=image)
-            
         return ad
 
-    # Метод для РЕДАКТИРОВАНИЯ объявления (PATCH)
     def update(self, instance, validated_data):
-        uploaded_images = validated_data.pop('uploaded_images', None)
+        uploaded_images = validated_data.pop('uploaded_images', [])
+        deleted_images = validated_data.pop('deleted_images', [])
         
-        # Обновляем обычные текстовые поля
+        # Обновляем тексты
         for attr, value in validated_data.items():
             setattr(instance, attr, value)
         instance.save()
         
-        # Если прилетели новые фотки — сохраняем их
+        # УДАЛЯЕМ ФОТКИ, если юзер нажал крестик на фронте
+        if deleted_images:
+            AdImage.objects.filter(id__in=deleted_images, ad=instance).delete()
+            
+        # Добавляем новые
         if uploaded_images:
             for image in uploaded_images:
                 AdImage.objects.create(ad=instance, image=image)
