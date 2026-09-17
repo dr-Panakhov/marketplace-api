9d7a9fc9a862f76426b1e4b8baaa611c22b24494
dr-Panakhov <rafael.panaxov14@gmail.com>
2026-09-17 15:40:22 +0400

    add profile

--- ads/views.py
+++ ads/views.py
@@ -1,6 +1,8 @@
 from rest_framework import viewsets, permissions
 from .models import Ad
 from .serializers import AdSerializer
+from rest_framework.decorators import action # <--- Добавили
+from rest_framework.response import Response   # <--- Добавили
 
 class AdViewSet(viewsets.ModelViewSet):
     queryset = Ad.objects.all().order_by('-created_at')
@@ -9,3 +11,9 @@ class AdViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
+    @action(detail=False, methods=['get'], permission_classes=[permissions.IsAuthenticated])
+    def me(self, request):
+        # Достаем объявы только того юзера, который делает запрос
+        my_ads = Ad.objects.filter(author=request.user).order_by('-created_at')
+        serializer = self.get_serializer(my_ads, many=True)
+        return Response(serializer.data)
