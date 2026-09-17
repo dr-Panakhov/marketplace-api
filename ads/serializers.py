edd857aae141063c48b967e39ec6c08c7351a5b6
dr-Panakhov <rafael.panaxov14@gmail.com>
2026-09-17 22:19:41 +0400

    feat(api): добавил обработку фоток при создании и редактировании объявлений

--- ads/serializers.py
+++ ads/serializers.py
@@ -8,9 +8,41 @@ class AdImageSerializer(serializers.ModelSerializer):
 
 class AdSerializer(serializers.ModelSerializer):
     images = AdImageSerializer(many=True, read_only=True)
-    author = serializers.ReadOnlyField(source='author.email') # Отдаем email вместо ID юзера
+    # Специальное поле, чтобы ловить файлы с фронтенда
+    uploaded_images = serializers.ListField(
+        child=serializers.ImageField(allow_empty_file=False, use_url=False),
+        write_only=True,
+        required=False
+    )
+    author = serializers.ReadOnlyField(source='author.email')
 
     class Meta:
         model = Ad
-        # Не забудь, что тут теперь есть currency!
-        fields = ['id', 'title', 'city', 'phone_number', 'description', 'price', 'currency', 'author', 'created_at', 'images']
+        # Не забудь, что тут теперь есть и images, и uploaded_images!
+        fields = ['id', 'title', 'city', 'phone_number', 'description', 'price', 'currency', 'author', 'created_at', 'images', 'uploaded_images']
+
+    # Метод для СОЗДАНИЯ объявления (POST)
+    def create(self, validated_data):
+        uploaded_images = validated_data.pop('uploaded_images', [])
+        ad = Ad.objects.create(**validated_data)
+        
+        for image in uploaded_images:
+            AdImage.objects.create(ad=ad, image=image)
+            
+        return ad
+
+    # Метод для РЕДАКТИРОВАНИЯ объявления (PATCH)
+    def update(self, instance, validated_data):
+        uploaded_images = validated_data.pop('uploaded_images', None)
+        
+        # Обновляем обычные текстовые поля
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        instance.save()
+        
+        # Если прилетели новые фотки — сохраняем их
+        if uploaded_images:
+            for image in uploaded_images:
+                AdImage.objects.create(ad=instance, image=image)
+                
+        return instance
